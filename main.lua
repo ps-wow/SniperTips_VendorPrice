d4df42f36f01b6b784db2b9e013d38d6257aa9fe
davidcraig <david@davidcraig.eu>
2019-07-14 19:39:17 +0100

    Update to SniperTips lib version 1.1

--- main.lua
+++ main.lua
@@ -1,18 +1,17 @@
 local SniperTips_VendorPrice = LibStub("AceAddon-3.0"):NewAddon('SniperTips_VendorPrice');
 
-local LibTooltip = LibStub("SniperTips-1.0");
+local LibTooltip = LibStub("SniperTips-1.1");
 
 local tipColour = { 0.2, 0.6, 1 }
 
-function SniperTips_VendorPrice:HandleItem(self, itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,
-  itemEquipLoc, itemIcon, itemSellPrice, itemClassID, itemSubClassID, bindType, expacID, itemSetID, isCraftingReagent)
-  if itemSellPrice ~= nil then
-    if (itemSellPrice == 0) then
+function SniperTips_VendorPrice:HandleItem(self, item)
+  if item.sellPrice ~= nil then
+    if (item.sellPrice == 0) then
       -- do nothing
     else
-      count = GetItemCount(itemLink);
-      priceEach = GetCoinTextureString(itemSellPrice);
-      priceAll = GetCoinTextureString(itemSellPrice * count);
+      count = GetItemCount(item.link);
+      priceEach = GetCoinTextureString(item.sellPrice);
+      priceAll = GetCoinTextureString(item.sellPrice * count);
 
       if (count > 1) then
         self:AddDoubleLine("Vendor Sell (All): ",priceAll,unpack(tipColour));
